0e7edb892fa16d05a7ef188c920315569b8b0a74
Goreev Artyom <81591047+GoreevArtem@users.noreply.github.com>
2023-04-15 16:07:02 +0300

    Update db.py

--- db.py
+++ db.py
@@ -21,25 +21,3 @@ def QUERY(connection, query):
             connection.commit()
     except (Exception, Error) as error:
         print("[ERROR] PostgreSQL", error)
-
-
-if __name__ == "__main__":
-    try:
-        connection = psycopg2.connect("postgres://icwsptef:gInfaMD89g87DZbN7gUGPpROyg7UQvCD@ziggy.db.elephantsql.com/icwsptef")
-        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
-        
-        print(QUERY_GET(connection=connection, query = 'SELECT * FROM public."Company"'))
-        print(QUERY_GET(connection=connection, query = """SELECT * FROM public."Employee" WHERE full_name = 'werw rwer'""", param = True))
-
-        QUERY(connection=connection, query="""INSERT INTO public."Employee" VALUES (1, 'Galaxy S9', 'Samsung', '4', '63000')""")
-
-        QUERY(connection=connection, query="""UPDATE public."Employee" SET id = id + 3000;""")
-        QUERY(connection=connection, query="""DELETE FROM public."Employee" WHERE full_name='Samsung';""")
-
-
-    except (Exception, Error) as error:
-        print("[ERROR] PostgreSQL", error)
-    finally:
-        if connection:
-            connection.close()
